fc7a7ed5af42791a83906f63cab53d794942e18c
tz5514 <tz5514@gmail.com>
2017-02-20 03:17:19 +0800

    註冊會員條款

--- entry/App/containers/AppModalContainer.jsx
+++ entry/App/containers/AppModalContainer.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import MemberLoginModal from 'Member/components/MemberLoginModal'
+import MemberTermsModal from 'Member/components/MemberTermsModal'
 
 class AppModalContainer extends Component {
   render() {
     return (
       <div>
         <MemberLoginModal/>
+        <MemberTermsModal/>
       </div>
     )
   }
