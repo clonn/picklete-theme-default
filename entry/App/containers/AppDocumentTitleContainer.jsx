c7694ef375623d0ca15c8742e78747cbca3c6d5b
tz5514 <tz5514@gmail.com>
2017-02-20 21:31:03 +0800

    route title 改定義在 routes.jsx

--- entry/App/containers/AppDocumentTitleContainer.jsx
+++ entry/App/containers/AppDocumentTitleContainer.jsx
@@ -3,66 +3,23 @@ import { connect } from 'react-redux'
 import Helmet from "react-helmet"
 
 class AppDocumentTitleContainer extends Component {
-  getRoutePath = () => '/' + _.flatMap(this.props.routes, (obj) => obj.path).slice(1).filter((e) => e).join('/')
-
   getDocumentTitle() {
-    const path = this.getRoutePath();
-    switch (path) {
-      case '/':
-        return ''
-      case '/cart':
-        return '購物車'
-      case '/checkout/complete':
-        return '結帳購買 - 結帳完成'
-      case '/checkout/information':
-        return '結帳購買 - 結帳資訊'
-      case '/member/register':
-        return '會員專區 - 註冊會員'
-      case '/member/orders':
-        return '會員專區 - 訂單管理'
-      case '/shop/department/:dptID(/:subDptID)':
-        return this.getDepartmentPageTitle()
-      case '/shop/product/:productID':
-        return this.getProductPageTitle()
-    }
-  }
-
-  getDepartmentPageTitle() {
-    let data = this.props.products.list.data;
-    const {dptID, subDptID} = this.props.params;
-
-    if (subDptID) {
-      data = data.filter((product) => product.subDpt.includes(parseInt(subDptID)));
-    } else {
-      data = data.filter((product) => product.dpt.includes(parseInt(dptID)));
-    }
-    
-    let titleStr = '商品訂購'
-    if (this.props.dpts.status == "success") {
-      const dpt = this.props.dpts.data.filter(dpt => dpt.id == dptID)[0];
-      const subDpt = (subDptID)? dpt.DptSubs.filter(sd => sd.id == subDptID)[0] : null;
-      titleStr += ` - ${dpt.name} ${(subDptID)? ` - ${subDpt.name}` : ''}`;
-    }
-    return titleStr;
-  }
-
-  getProductPageTitle() {
-    let product = this.props.products.detail[this.props.params.productID];
-    return '商品訂購' + ((product && product.data)? ` - ${product.data.name}` : '');
+    let title = _.last(this.props.routes).getTitle(this.props.state, this.props.params);
+    return ((title)? `${title} / ` : '') + (this.props.state.company.data.name || '');
   }
 
   render() {
-    let title = this.getDocumentTitle();
-    title = ((title)? `${title} / ` : '') + (this.props.company.data.name || '');
     return (
-      <Helmet title={title}/>
+      <Helmet title={this.getDocumentTitle()}/>
     );
   }
 }
 
 export default connect(state => ({
-  member: state.member,
-  company: state.company,
-  products: state.products,
-  dpts: state.dpts,
+  state: {
+    member: state.member,
+    company: state.company,
+    products: state.products,
+    dpts: state.dpts,
+  }
 }))(AppDocumentTitleContainer);
--- root/routes.jsx
+++ root/routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, IndexRoute, Redirect } from 'react-router'
 
-
 import App from 'App/pages/App'
 import Home from 'Home/pages/Home'
 
@@ -21,25 +20,50 @@ import CheckoutCompleteContainer from 'Checkout/containers/CheckoutCompleteConta
 
 import MemberRegister from 'Member/components/MemberRegister'
 
+
+const getProductPageTitle = (state, params) => {
+  let product = state.products.detail[params.productID];
+  return '商品訂購' + ((product && product.data)? ` - ${product.data.name}` : '');
+}
+
+const getDepartmentPageTitle = (state, params) => {
+  let data = state.products.list.data;
+  const {dptID, subDptID} = params;
+
+  if (subDptID) {
+    data = data.filter((product) => product.subDpt.includes(parseInt(subDptID)));
+  } else {
+    data = data.filter((product) => product.dpt.includes(parseInt(dptID)));
+  }
+  
+  let titleStr = '商品訂購'
+  if (state.dpts.status == "success") {
+    const dpt = state.dpts.data.filter(dpt => dpt.id == dptID)[0];
+    const subDpt = (subDptID)? dpt.DptSubs.filter(sd => sd.id == subDptID)[0] : null;
+    titleStr += ` - ${dpt.name} ${(subDptID)? ` - ${subDpt.name}` : ''}`;
+  }
+  return titleStr;
+}
+
 export default (
   <Route path="/" component={App}>
-    <IndexRoute name="home" component={Home}/>
+    <IndexRoute name="home" component={Home} getTitle={() => ''}/>
     <Route path="shop" component={Shop}>
-      <Route path="department/:dptID(/:subDptID)" component={ShopProductListContainer}/>
-      <Route path="product/:productID" component={ShopProductDetailContainer}/>
+      <Route path="department/:dptID(/:subDptID)" component={ShopProductListContainer} getTitle={getDepartmentPageTitle}/>
+      <Route path="product/:productID" component={ShopProductDetailContainer} getTitle={getProductPageTitle}/>
     </Route>
     <Route path="cart" component={Cart}>
-      <IndexRoute component={CartContainer}/>
+      <IndexRoute component={CartContainer} getTitle={() => '購物車'}/>
     </Route>
-    <Route path="member/register" component={MemberRegister}/>
+    <Route path="member/register" component={MemberRegister} getTitle={() => '會員專區 - 註冊會員'}/>
 
     <Route path="member" component={Member}>
-      <Route path="orders" component={MemberOrderContainer}/>
+      <Route path="orders" component={MemberOrderContainer} getTitle={() => '會員專區 - 訂單管理'}/>
     </Route>
 
     <Route path="checkout" component={Checkout}>
-      <Route path="information" component={CheckoutInfoContainer}/>
-      <Route path="complete" component={CheckoutCompleteContainer}/>
+      <Route path="information" component={CheckoutInfoContainer} getTitle={() => '結帳購買 - 結帳資訊'}/>
+      <Route path="complete" component={CheckoutCompleteContainer} getTitle={() => '結帳購買 - 結帳完成'}/>
     </Route>
   </Route>
 );
